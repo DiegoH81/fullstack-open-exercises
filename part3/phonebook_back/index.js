793f58b37bf5bdef5d8732328dfa7f2e8da35dc9
DiegoH81 <hidal8909ins@gmail.com>
2026-08-13 13:47:27 -0500

    feat(part3): add .env port

--- part3/phonebook_back/index.js
+++ part3/phonebook_back/index.js
@@ -97,7 +97,9 @@ app.post("/api/persons", (request, response) => {
 
     response.status(200).json(new_person);
 })
-const PORT = 3001
+
+const PORT = process.env.PORT || 3001
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
