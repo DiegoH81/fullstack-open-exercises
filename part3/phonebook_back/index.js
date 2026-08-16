6ca8ac4a57a0434d605348fd2c1a76daffb493da
DiegoH81 <hidal8909ins@gmail.com>
2026-08-16 16:05:43 -0500

    feat(part3): add exercises from 3.14-3.18

--- part3/phonebook_back/index.js
+++ part3/phonebook_back/index.js
@@ -72,6 +72,17 @@ app.post("/api/persons", (request, response) => {
       })
 })
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler);
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
